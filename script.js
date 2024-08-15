b139401850c37f276a8a5040983948fc44fd4a55
Thakur-Khushi <thakurramkhushi@gmail.com>
2024-08-15 22:09:14 +0530

    added the event listner in js file for big circle and icon block

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const bigCircle = document.querySelector(".big-circle");
+    const iconBlocks = document.querySelectorAll(".icon-block");
+  
+    // Function to stop animations
+    function stopAnimations() {
+      document.querySelector(".main").classList.add("stop-rotation");
+    }
+  
+    // Function to start animations
+    function startAnimations() {
+      document.querySelector(".main").classList.remove("stop-rotation");
+    }
+  
+    // Add event listeners
+    bigCircle.addEventListener("mouseover", stopAnimations);
+    bigCircle.addEventListener("mouseout", startAnimations);
+  
+    iconBlocks.forEach(icon => {
+      icon.addEventListener("mouseover", stopAnimations);
+      icon.addEventListener("mouseout", startAnimations);
+    });
+  });
+  
